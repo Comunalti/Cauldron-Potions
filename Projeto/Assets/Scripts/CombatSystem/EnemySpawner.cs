d55d72b0651cf1c384ae2e1fde71e857bdbf2b1c
Comunalti <frps2003@gmail.com>
2021-10-10 20:12:26 -0300

    xcafasdfsdfg

--- Projeto/Assets/Scripts/CombatSystem/EnemySpawner.cs
+++ Projeto/Assets/Scripts/CombatSystem/EnemySpawner.cs
@@ -1,16 +1,29 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace CombatSystem
 {
-    public class EnemySpawner
+    public class EnemySpawner : MonoBehaviourSingleton<EnemySpawner>
     {
         public List<GameObject> Prefabs = new List<GameObject>();
-        //public 
+        public Vector3 spawnPosition;
+        public Vector3 stopPosition;
+        public float walkDuration;
+        
         public void CreateNewEnemy()
         {
-            var nextEnemy = Prefabs.GetRandom();
+            var nextEnemy = Instantiate(Prefabs.GetRandom(),spawnPosition,Quaternion.identity);
+            BattleManager.Instance.AddCreatureToFight(nextEnemy.GetComponent<Creature>());
             
+            nextEnemy.transform.DOMove(stopPosition,walkDuration).OnComplete(() => { StartCoroutine(BattleManager.Instance.Fight()); });
+        }
+
+        public IEnumerator NextEnemy()
+        {
+            yield return new WaitForSeconds(1);
         }
     }
 }
