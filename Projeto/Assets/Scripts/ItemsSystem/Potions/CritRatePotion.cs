f95adee47c9db87da04aada63020f4b34f633d13
MalcolnLMR <92189383+MalcolnLMR@users.noreply.github.com>
2021-10-11 01:22:44 -0300

    asdasda

--- Projeto/Assets/Scripts/ItemsSystem/Potions/CritRatePotion.cs
+++ Projeto/Assets/Scripts/ItemsSystem/Potions/CritRatePotion.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 namespace ItemsSystem.Potions
 {
-    public class CritDamagePotion : Potion
+    public class CritRatePotion : Potion
     {
-        public float critDamage = 0.1f;
+        public float critRateChange = 0.1f;
         protected override IEnumerator MainEffect(Creature creature)
         {
             yield return new WaitForSeconds(1);
-            creature.AddCritDamage(critDamage);
+            creature.AddCritRate(critRateChange);
             yield break;
         }
     }
