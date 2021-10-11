c29bfb12c221dfb1507d7840394205d3e4c530bf
Comunalti <frps2003@gmail.com>
2021-10-11 01:39:57 -0300

    adssad

--- Projeto/Assets/Scripts/PotionSystem/PotionResult.cs
+++ Projeto/Assets/Scripts/PotionSystem/PotionResult.cs
@@ -27,7 +27,7 @@ namespace PotionSystem
 
         public GameObject GetInstance(Vector3 position)//GameObject goodPrefab,GameObject badPrefab
         {
-            GameObject clone = Instantiate(Prefab); //isGood?Instantiate(goodPrefab,position,Quaternion.identity):Instantiate(badPrefab,position,Quaternion.identity);
+            GameObject clone = Instantiate(Prefab,position,Quaternion.identity); //isGood?Instantiate(goodPrefab,position,Quaternion.identity):Instantiate(badPrefab,position,Quaternion.identity);
             
             //var child = clone.GetChildWithName("PotLiquido");
             //child.GetComponent<SpriteRenderer>().color = color;
