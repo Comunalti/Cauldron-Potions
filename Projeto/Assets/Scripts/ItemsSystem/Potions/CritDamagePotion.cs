f268a9d1d12479e97cc8d820ca102d78ca478704
MalcolnLMR <92189383+MalcolnLMR@users.noreply.github.com>
2021-10-10 22:29:10 -0300

    aaaaaaaaa

---
+++ Projeto/Assets/Scripts/ItemsSystem/Potions/CritDamagePotion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using CombatSystem;
+using UnityEngine;
+
+namespace ItemsSystem.Potions
+{
+    public class CritRatePotion : Potion
+    {
+        public float critRateChange = 2;
+        protected override IEnumerator MainEffect(Creature creature)
+        {
+            yield return new WaitForSeconds(1);
+            creature.AddCritRate(critRateChange);
+            yield break;
+        }
+    }
+}
---
+++ Projeto/Assets/Scripts/ItemsSystem/Potions/CritRatePotion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using CombatSystem;
+using UnityEngine;
+
+namespace ItemsSystem.Potions
+{
+    public class CritDamagePotion : Potion
+    {
+        public float CritDamagePotion = 10;
+        protected override IEnumerator MainEffect(Creature creature)
+        {
+            yield return new WaitForSeconds(1);
+            creature.AddCritDamage(CritDamagePotion);
+            yield break;
+        }
+    }
+}
